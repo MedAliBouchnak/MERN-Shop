65e4449e396dd30a0c59268265cbc1e2606c9c60
MedAliBouchnak <106705873+MedAliBouchnak@users.noreply.github.com>
2023-05-05 18:06:38 +0100

    Impelementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router , Route, Routes} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import HomeScreens from './screens/HomeScreens'
+import ProductScreen from './screens/ProductScreen'
+
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
-        <Container>
-          <HomeScreens />
+        <Container> <Routes>
+          <Route path='/' Component={HomeScreens} exact/>
+          <Route path='/product/:id' Component={ProductScreen} /></Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
